a66809e2a94fa0e62749dc21c64e75858fbad20c
Anastasia <anastasia_martiniuc@homedepot.com>
2023-12-28 16:27:29 +0200

    Initial Commit

---
+++ apple-frameworks/apple_frameworksApp.swift
@@ -0,0 +1,17 @@
+//
+//  apple_frameworksApp.swift
+//  apple-frameworks
+//
+//  Created by amartiniuc on 28.12.2023.
+//
+
+import SwiftUI
+
+@main
+struct apple_frameworksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
