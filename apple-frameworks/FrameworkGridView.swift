8069b542a3b21162ca079757c66cf0168312b1b3
Anastasia <anastasia_martiniuc@homedepot.com>
2023-12-28 17:17:42 +0200

    feat: init frameworks app

---
+++ apple-frameworks/FrameworkGridView.swift
@@ -0,0 +1,54 @@
+//
+//  FrameworkGridView.swift
+//  apple-frameworks
+//
+//  Created by amartiniuc on 28.12.2023.
+//
+
+import SwiftUI
+
+struct FrameworkGridView: View {
+    
+    let columns: [GridItem] = [GridItem(.flexible()), 
+                               GridItem(.flexible()),
+                               GridItem(.flexible())]
+    
+    var body: some View {
+        NavigationView {
+            ScrollView {
+                LazyVGrid(columns: columns) {
+                    ForEach(MockData.frameworks) { framework in
+                        FrameworkTitleView(framework: framework)
+                    }
+                }
+            }
+            .navigationTitle("🍎 Frameworks")
+        }
+    }
+}
+
+struct FrameworkGridView_Preview: PreviewProvider{
+    static var previews: some View{
+        FrameworkGridView()
+            .preferredColorScheme(.dark)
+    }
+}
+
+struct FrameworkTitleView: View {
+    
+    let framework: Framework
+    
+    var body: some View {
+        VStack{
+            Image(framework.imageName)
+                .resizable()
+                .frame(width: 90, height: 90)
+            Text(framework.name)
+                .font(.title2)
+                .fontWeight(.semibold)
+                .scaledToFit()
+                .minimumScaleFactor(0.6)
+        }
+        .padding()
+    }
+}
